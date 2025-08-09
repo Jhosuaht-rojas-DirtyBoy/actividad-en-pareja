20e2c287e0e40500b63b47638d6a81e20afb726c
Juan Santos <juanbenjaminsantosgonzalez@gmail.com>
2025-08-08 21:02:46 -0500

    Primera parte codigo calculadora

--- Funciones.py
+++ Funciones.py
@@ -1,14 +1,4 @@
-def multiplicacion():
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
-    resultado = num1 * num2
-    print(f"El resultado de la multiplicación es {resultado}")
-
-def division():
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
-    if num2 != 0:
-        resultado = num1 / num2
-        print(f"El resultado de la división es {resultado}")
-    else:
-        print("Error: No se puede dividir entre cero.")
+def suma(num1, num2):
+    return num1 + num2
+def resta(num1, num2):
+    return num1 - num2
